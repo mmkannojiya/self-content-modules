a3a07448248a9ca59661131049e04b7e305d05d2
Madanmohan Kannojiya <madanmohan.kannojiya@T-Mobile.com>
2016-06-22 10:28:24 +0530

    error fixes

--- login-module-config.js
+++ login-module-config.js
@@ -1,16 +1,21 @@
 angular.module('myTmoApp.loginModule',['ui.router']);
-angular.module('myTmoApp.loginModule').config(function ($stateProvider) {
-    $stateProvider
+angular.module('myTmoApp.loginModule').config(function ($stateProvider ,$locationProvider, $urlRouterProvider) {
+  //$urlRouterProvider.otherwise("/");
+  $locationProvider.html5Mode(true).hashPrefix('!');
+  $stateProvider
       .state('login', {
         url: '/login',
 		template:'<h1>Hey There</h1>'
       });
 
-  }).constant('LOGIN_CONST':{
-	'PASSWORD_INVALID_MSG': 'Username/Password is incorrect',
-    'SECURE_PROTOCOL': 'https',
-    'HTTP_PROTOCOL': 'http',
+  }).constant('LOGIN_CONST', {
+    'LOGIN': '/auth',
     'AUTHLOGINDATA': 'authlogindata',
-    'TOKEN_INITIALS': 'xyz',
-    'REP': 'rep'
-	});
+    'DOUBLESLASH': '://',
+    'CLIENT_CREDENTIALS': 'client_credentials',
+    'ENTER_MSISDN': 'Enter a value for MSISDN',
+    'ENETR_EMAIL': 'Enter a value for email',
+    'LOGGEDIN_STATE': 'loggedinState',
+    'LOGGEDIN_STATE_PARAMS': 'loggedinStateParams',
+    //E2E Fix Start - QC28265, QC28380- Switch Accounts Link missing
+	'SET_MULTIPLE_ACCOUNTS_EVENT': 'setMultipleAccounts'});
